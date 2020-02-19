99cc318e8b29855a52cc13d085aea592663e055c
hwluo12.shijingshan <hwluo12@163.com>
2020-02-19 14:19:45 +0800

    路由配置

--- config/config.js
+++ config/config.js
@@ -15,11 +15,19 @@ export default {
       routes: [
         {
           path: "/",
-          redirect: "/helloworld"
+          component: "./helloworld"
         },
         {
           path: "/helloworld",
           component: "./helloworld"
+        },
+        {
+          path: "/dashboard",
+          routes: [
+            { path: "/dashboard/analysis", component: "./Dashboard/Analysis" },
+            { path: "/dashboard/monitor", component: "./Dashboard/Monitor" },
+            { path: "/dashboard/workplace", component: "./Dashboard/Workplace" }
+          ]
         }
       ]
     }
