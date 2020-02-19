fb0b8e124ac4f97ba7be5478190747c0ccfe91a9
hwluo12.shijingshan <hwluo12@163.com>
2020-02-19 15:52:22 +0800

    新增puzzlecards的model

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,45 +1,34 @@
 import React, { Component } from "react";
 import { Card, Button } from "antd";
+import { connect } from "dva";
 
-export default class PuzzleCards extends Component {
-  constructor(props) {
-    super(props);
-    this.counter = 100;
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: "Did you hear about the two silk worms in a race?",
-          punchline: "It ended in a tie"
-        },
-        {
-          id: 2,
-          setup: "What happens to a frog's car when it breaks down?",
-          punchline: "It gets toad away"
-        }
-      ]
-    };
-  }
+const namespace = "puzzlecards";
 
-  addNewCard = () => {
-    this.setState(prevState => {
-      const prevCardList = prevState.cardList;
-      this.counter += 1;
-      const card = {
-        id: this.counter,
-        setup: "this is a test setup",
-        punchline: "this is a test punchline"
-      };
-      return {
-        cardList: prevCardList.concat(card)
-      };
-    });
+const mapStateToProps = state => {
+  const cardList = state[namespace].data;
+  return {
+    cardList
   };
+};
 
+const mapDispatchToProps = dispatch => {
+  return {
+    onClickAdd: newCard => {
+      const action = {
+        type: `${namespace}/addNewCard`,
+        payload: newCard
+      };
+      dispatch(action);
+    }
+  };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class PuzzleCards extends Component {
   render() {
     return (
       <div>
-        {this.state.cardList.map(card => {
+        {this.props.cardList.map(card => {
           return (
             <Card key={card.id}>
               <div>Q: {card.setup}</div>
@@ -50,7 +39,17 @@ export default class PuzzleCards extends Component {
           );
         })}
         <div>
-          <Button onClick={this.addNewCard}>添加卡片</Button>
+          <Button
+            onClick={() =>
+              this.props.onClickAdd({
+                setup:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                punchline: "here we use dva"
+              })
+            }
+          >
+            添加卡片
+          </Button>
         </div>
       </div>
     );
