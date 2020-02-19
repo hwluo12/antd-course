e732311c515a8ad962c6ff513256a1f33c2d127f
hwluo12.shijingshan <hwluo12@163.com>
2020-02-19 17:17:43 +0800

    删除puzzlecards模拟数据

--- src/model/puzzlecards.js
+++ src/model/puzzlecards.js
@@ -9,18 +9,7 @@ const delay = millsecond => {
 export default {
   namespace: "puzzlecards",
   state: {
-    data: [
-      {
-        id: 1,
-        setup: "Did you hear about the two silk worms in a race?",
-        punchline: "It ended in a tie"
-      },
-      {
-        id: 2,
-        setup: "What happens to a frog's car when it breaks down?",
-        punchline: "It gets toad away"
-      }
-    ],
+    data: [],
     counter: 100
   },
   effects: {
